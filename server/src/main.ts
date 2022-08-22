e81184b59f8ee8a383d4f3dae88c0710f63204e0
shahin1290 <s.patowary@yahoo.com>
2022-08-22 12:37:13 +0200

    adds logger and mongoose connection set up

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,29 @@
 import express from "express";
+import { connectToDatabase, disconnectFromDatabase } from "./utils/database";
+import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listenint at http://localhost: ${PORT}`);
+const server = app.listen(PORT, async () => {
+  await connectToDatabase();
+  logger.info(`Server listenint at http://localhost: ${PORT}`);
 });
 
 const signals = ["SIGTERM", "SIGINT"];
 
 const gracefulShutdown = (signal: string) => {
   process.on(signal, async () => {
-    console.log("Goodbye, got signal", signal);
+    logger.info("Goodbye, got signal", signal);
 
     server.close();
 
+    await disconnectFromDatabase();
+
     //disconnect from db
 
-    console.log("My work here is done");
+    logger.info("My work here is done");
 
     process.exit(0);
   });
